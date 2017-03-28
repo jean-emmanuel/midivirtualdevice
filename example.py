6283c2199295c32afb39d1fa74df4a4c7c292e0d
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-03-28 08:38:46 +0200

    example: doc

--- example.py
+++ example.py
@@ -1,26 +1,29 @@
 #encoding: utf-8
 
 from pyo import *
-
 from midivirtualdevice import MidiVirtualDevice
-
 from time import sleep
 
+# Create a virtual midi device with one set of i/o named test
+
 virtual = MidiVirtualDevice(['test'])
+
+
+# Listen on virtual.ports['test']['out'] and print received midi
 
 def midicall(*args):
     print(args)
 
 listen = MidiListener(midicall, virtual.ports['test']['in'])
+listen.start()
+
+# Send a NoteOn/Off every second on virtual.ports['test']['out']
 
 dispatch = MidiDispatcher(virtual.ports['test']['out'])
-
-listen.start()
 dispatch.start()
 
-print('MidiVirtualDevice is running (ctr+c to stop)')
-
 try:
+    print('MidiVirtualDevice is running (ctr+c to stop)')
     while True:
         dispatch.send(144, 64, 64)
         dispatch.send(128, 64, 64)
